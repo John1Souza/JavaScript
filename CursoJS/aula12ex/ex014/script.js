e5e9e91d32aa2e8f79cd318d6ea2ead9b454b688
John1Souza <109979241+John1Souza@users.noreply.github.com>
2022-08-07 21:15:19 -0300

    criei os exercicios 14 e 15

--- CursoJS/aula12ex/ex014/script.js
+++ CursoJS/aula12ex/ex014/script.js
@@ -1,1 +1,21 @@
-alert( 'Olá!')
+function carregar(){
+    var msg = window.document.getElementById('msg')
+    var img = window.document.getElementById('imagem')  
+    var data = new Date()
+    //var hora = data.getHours()
+    var hora = 20
+    msg.innerHTML = `Agora são ${hora} horas`
+    if (hora >= 0 && hora < 12) {
+        //BOM DIA!
+        img.src = 'dia_1.png'
+        document.body.style.background = '#e3735b'
+    } else if (hora >= 12 && hora <= 18) {
+        //BOA TARDE!
+        img.src = 'tarde_1.png'
+        document.body.style.background = '#1e3c3c'
+    } else {
+        //BOA NOITE!
+        img.src = 'noite_1.png'
+        document.body.style.background = '001626'
+    }
+}
