3e535d2bd7d122310e1d68affb6fbda98c68bb25
John1Souza <109979241+John1Souza@users.noreply.github.com>
2022-08-09 14:58:26 -0300

    aula 17

---
+++ CursoJS/aula17/ambiente.js
@@ -0,0 +1,11 @@
+let amigo = {nome: 'jose',
+sexo: 'm',
+peso: 85.4,
+engordar(p){
+    console.log('Engordar')
+    this.peso += p
+}
+
+}
+amigo.engordar(2)
+console.log(`${amigo.nome} pesa ${amigo.peso}Kg`)
